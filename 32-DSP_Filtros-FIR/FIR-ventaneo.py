6d04fadea9e508fd3b06ef110eb72e44035a5899
Exequiel25 <exequiel.juan.magni@gmail.com>
2024-11-19 00:20:52 +0000

    Mejora del FIR por ventaneo

--- 32-DSP_Filtros-FIR/FIR-ventaneo.py
+++ 32-DSP_Filtros-FIR/FIR-ventaneo.py
@@ -2,13 +2,15 @@ import numpy as np
 from scipy.signal import firwin
 
 # Parámetros del filtro
-orden = 50
-frecuencia_corte = 2000  # Hz
+orden = 30
+frecuencia_corte = [2000]
 frecuencia_muestreo = 44100  # Hz
+n_coeficientes = 25
 beta = 6.5
 
 # Normalización de la frecuencia de corte
-fc_normalizada = frecuencia_corte / (frecuencia_muestreo / 2)
+fc_normalizada = [f_corte / (frecuencia_muestreo / 2)
+                  for f_corte in frecuencia_corte]
 
 # Cálculo de los coeficientes del filtro FIR usando la ventana de Kaiser
 coeff_kaiser = firwin(orden + 1, fc_normalizada, window=('kaiser', beta))
@@ -20,9 +22,9 @@ coeff_hamming = firwin(orden + 1, fc_normalizada, window='hamming')
 coeff_blackman = firwin(orden + 1, fc_normalizada, window='blackman')
 
 # Sumar los primeros 40 elementos del vector coeff para cada ventana
-resp_kaiser = np.sum(coeff_kaiser[:40])
-resp_hamming = np.sum(coeff_hamming[:40])
-resp_blackman = np.sum(coeff_blackman[:40])
+resp_kaiser = np.sum(coeff_kaiser[:n_coeficientes])
+resp_hamming = np.sum(coeff_hamming[:n_coeficientes])
+resp_blackman = np.sum(coeff_blackman[:n_coeficientes])
 
 # Mostrar los resultados con 4 decimales
 print(f'La suma de los primeros 40 elementos con ventana de Kaiser es: {
